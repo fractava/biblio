771ccb84e4a612862d0f6eae1504dd47d8113a4b
TessyPowder <tessy.powder@gmail.com>
2020-07-20 12:24:59 +0200

    added vuex stores

---
+++ biblio-frontend/store/medias.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+  medias: [
+		{
+			id: 0,
+			name: "Mathe 11",
+			instances: [
+				{id: 0, barcode: "M11-1"},
+				{id: 1, barcode: "M11-2"},
+			],
+		},
+	],
+});
+
+export const mutations = {
+
+};
+
+export const actions = {
+
+};
