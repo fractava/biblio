7afbd1b5a063b248823ec481c2e9de709c55f541
TessyPowder <tessy.powder@gmail.com>
2020-07-20 09:52:38 +0200

    added navbar component

---
+++ biblio-frontend/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<md-tabs md-sync-route>
+			<md-tab id="tab-home" md-label="Medien E/A" to="" exact></md-tab>
+			<md-tab id="tab-catalog" md-label="Katalogisierung" to=""></md-tab>
+			<md-tab id="tab-students" md-label="Schüler" to=""></md-tab>
+			<md-tab id="tab-lists" md-label="Listen" to=""></md-tab>
+			<md-tab id="tab-admin" md-label="Verwaltung" to=""></md-tab>
+		</md-tabs>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "navbar"
+};
+</script>
