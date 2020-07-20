319032910f02c621e60b08fb43c491a7abd3acfc
TessyPowder <tessy.powder@gmail.com>
2020-07-20 11:31:42 +0200

    added two sections and a student select input to home

--- biblio-frontend/pages/home.vue
+++ biblio-frontend/pages/home.vue
@@ -1,13 +1,46 @@
 <template>
-  <p>Home</p>
+  <div class="md-layout md-gutter md-alignment-top-center">
+    <div class="md-layout-item md-medium-size-50 md-small-size-100 md-xsmall-size-100">
+			<md-card>
+				<md-card-header>
+					<div class="md-title">Zurückgeben</div>
+				</md-card-header>
+				<md-card-content>
+				</md-card-content>
+			</md-card>
+		</div>
+    <div class="md-layout-item md-medium-size-50 md-small-size-100 md-xsmall-size-100">
+			<md-card>
+				<md-card-header>
+					<div class="md-title">Ausleihen</div>
+				</md-card-header>
+				<md-card-content>
+					<md-autocomplete v-model="selectedStudent" :md-options="students">
+						<label>Student</label>
+						<template slot="md-autocomplete-item" slot-scope="{ item, term }">
+							<md-highlight-text :md-term="term">{{ item }}</md-highlight-text>
+						</template>
+
+						<template slot="md-autocomplete-empty" slot-scope="{ term }">
+							No students matching "{{ term }}" were found.
+						</template>
+					</md-autocomplete>
+				</md-card-content>
+			</md-card>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-	components: {
+	data: function() {
+		return {
+			selectedStudent: "",
+			students: [
+				"baum",
+				"test",
+			],
+		};
 	}
 };
 </script>
-
-<style>
-</style>
--- biblio-frontend/plugins/vue-material.js
+++ biblio-frontend/plugins/vue-material.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
-import { MdButton, MdContent, MdTabs } from "vue-material/dist/components";
+import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 
-Vue.use(MdButton);
-Vue.use(MdContent);
-Vue.use(MdTabs);
+Vue.use(VueMaterial);
