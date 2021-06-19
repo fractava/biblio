8e44eb387866ce1fe1687cd7320e31ff22bdafe0
Jonathan Treffler <mail@jonathan-treffler.de>
2021-06-19 14:13:32 +0000

    added files from nextcloud/app-tutorial

---
+++ biblio/lib/AppInfo/Application.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OCA\NotesTutorial\AppInfo;
+
+use OCP\AppFramework\App;
+
+class Application extends App {
+	public const APP_ID = 'biblio';
+
+	public function __construct() {
+		parent::__construct(self::APP_ID);
+	}
+}
