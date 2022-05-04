6f31c485c73211fc49365a502f5ac266a854c309
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2022-05-04 19:57:59 +0000

    added custom eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,23 @@
 module.exports = {
 	extends: [
 		'@nextcloud',
-	]
+	],
+	rules: {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
 }
