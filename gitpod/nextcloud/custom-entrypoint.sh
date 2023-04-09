0d0869cdfb54a72099bb93c0979ea82499f805b6
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-04-09 12:06:18 +0000

    hopefully fixed permissions problem

--- gitpod/nextcloud/custom-entrypoint.sh
+++ gitpod/nextcloud/custom-entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Both gitpod and Nextcloud need read/write acess
 # Obviously not recommended for a production system
+chown -R www-data:www-data /var/www/html/custom_apps/
 chmod -R 777 /var/www/html/custom_apps/
 
 /entrypoint.sh "$@"
