d30597c8ecac655c31da5fc11fbaad7cacb07143
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-09-20 15:39:38 +0000

    added libraries table to enable multiple seperated instances; added foreign key constraints

--- lib/Migration/Version000000Date20181013124731.php
+++ lib/Migration/Version000000Date20181013124731.php
@@ -11,6 +11,10 @@ use OCP\Migration\IOutput;
 
 class Version000000Date20181013124731 extends SimpleMigrationStep {
 
+    const LIBRARIES_TABLE = "biblio_libraries";
+    const MEDIUMS_TABLE = "biblio_mediums";
+	const MEDIUM_FIELDS_TABLE = "biblio_medium_fields";
+
 	/**
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
@@ -21,10 +25,48 @@ class Version000000Date20181013124731 extends SimpleMigrationStep {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
-		if (!$schema->hasTable('biblio_mediums')) {
-			$table = $schema->createTable('biblio_mediums');
+		if (!$schema->hasTable(self::LIBRARIES_TABLE)) {
+			$table = $schema->createTable(self::LIBRARIES_TABLE);
 			$table->addColumn('id', 'integer', [
 				'autoincrement' => true,
+				'notnull' => true,
+			]);
+			$table->addColumn('name', 'string', [
+				'notnull' => true,
+				'length' => 200,
+			]);
+
+			$table->setPrimaryKey(['id']);
+		}
+
+		if(!§schema->hasTable('biblio_library_members')) {
+			$table = $schema->createTable('biblio_library_members');
+			$table->addColumn('id', 'integer', [
+				'autoincrement' => true,
+				'notnull' => true,
+			]);
+			$table->addColumn('library_id', 'integer', [
+				'notnull' => true,
+			]);
+			$table->addColumn('user_id', 'string', [
+				'notnull' => true,
+				'length' => 200,
+			]);
+
+			// TODO: Permissions
+
+			$table->setPrimaryKey(['id']);
+			$table->addIndex(['library_id'], 'library_id_index');
+			$table->addForeignKeyConstraint($schema->getTable(self::LIBRARIES_TABLE), ['library_id'], ['id'], ['onDelete' => 'CASCADE'], 'members_library_id_fk');
+		}
+
+		if (!$schema->hasTable(self::MEDIUMS_TABLE)) {
+			$table = $schema->createTable(self::MEDIUMS_TABLE);
+			$table->addColumn('id', 'integer', [
+				'autoincrement' => true,
+				'notnull' => true,
+			]);
+			$table->addColumn('library_id', 'integer', [
 				'notnull' => true,
 			]);
 			$table->addColumn('title', 'string', [
@@ -34,17 +76,14 @@ class Version000000Date20181013124731 extends SimpleMigrationStep {
 			$table->addColumn('fields_order', 'string', [
 				'notnull' => true,
 			]);
-			$table->addColumn('user_id', 'string', [
-				'notnull' => true,
-				'length' => 200,
-			]);
 
 			$table->setPrimaryKey(['id']);
-			$table->addIndex(['user_id'], 'userIdIndex');
+			$table->addIndex(['library_id'], 'library_id_index');
+			$table->addForeignKeyConstraint($schema->getTable(self::LIBRARIES_TABLE), ['library_id'], ['id'], ['onDelete' => 'CASCADE'], 'mediums_library_id_fk');
 		}
 
-		if (!$schema->hasTable('biblio_medium_fields')) {
-			$table = $schema->createTable('biblio_medium_fields');
+		if (!$schema->hasTable(self::MEDIUM_FIELDS_TABLE)) {
+			$table = $schema->createTable(self::MEDIUM_FIELDS_TABLE);
 			$table->addColumn('id', 'integer', [
 				'autoincrement' => true,
 				'notnull' => true,
@@ -66,6 +105,7 @@ class Version000000Date20181013124731 extends SimpleMigrationStep {
 
 			$table->setPrimaryKey(['id']);
 			$table->addIndex(['medium_id'], 'mediumIdIndex');
+			$table->addForeignKeyConstraint($schema->getTable(self::MEDIUMS_TABLE), ['medium_id'], ['id'], ['onDelete' => 'CASCADE'], 'medium_fields_medium_id_fk');
 		}
 		return $schema;
 	}
