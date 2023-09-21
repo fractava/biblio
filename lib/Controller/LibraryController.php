f14fce4f7116f6fde8f89f973feaba206a6eae23
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-09-21 21:31:47 +0000

    added LibraryController

---
+++ lib/Controller/LibraryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace OCA\Biblio\Controller;
+
+use OCA\Biblio\AppInfo\Application;
+use OCA\Biblio\Service\LibraryService;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\IRequest;
+
+class LibraryController extends Controller {
+	/** @var LibraryService */
+	private $service;
+
+	/** @var string */
+	private $userId;
+
+	use Errors;
+
+	public function __construct(IRequest $request,
+                                LibraryService $service,
+								$userId) {
+		parent::__construct(Application::APP_ID, $request);
+		$this->service = $service;
+		$this->userId = $userId;
+	}
+
+	/**
+	 * Get all libraries the current user has access to
+	 * @NoAdminRequired
+     * @NoCSRFRequired
+	 */
+	public function index(): DataResponse {
+		return new DataResponse($this->service->findAll($this->userId));
+	}
+
+	/**
+	 * Get specific library
+	 * @NoAdminRequired
+     * @NoCSRFRequired
+	 */
+	public function show(int $libraryId): DataResponse {
+		return $this->handleNotFound(function () use ($libraryId) {
+			return $this->service->find($libraryId);
+		});
+	}
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function create(string $name): DataResponse {
+		return new DataResponse($this->service->create($name, $this->userId));
+	}
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function update(int $libraryId, string $name): DataResponse {
+		return $this->handleNotFound(function () use ($libraryId, $name) {
+			return $this->service->update($libraryId, $name);
+		});
+	}
+
+	/**
+	 * @NoAdminRequired
+	 */
+	public function destroy(int $libraryId): DataResponse {
+		return $this->handleNotFound(function () use ($libraryId) {
+			return $this->service->delete($libraryId);
+		});
+	}
+}
