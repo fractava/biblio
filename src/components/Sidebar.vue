f733ec70c79b5b6c36a386dccd56b7effbe5522c
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-09-22 13:15:07 +0000

    improved Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,21 +1,53 @@
 <template>
 	<NcAppNavigation>
-		<AppNavigationSelect />
-		<NcAppNavigationItem :title="t('biblio', 'Lend/Return')" to="/lend-return" icon="icon-medium" />
-		<NcAppNavigationItem :title="t('biblio', 'Mediums')" to="/mediums" icon="icon-category-enabled" />
-		<NcAppNavigationItem :title="t('biblio', 'Customers')" to="/customers" icon="icon-category-enabled" />
+		<AppNavigationSelect style="margin-bottom: 22px;">
+			<template #icon>
+				<LibraryShelves :size="20" />
+			</template>
+			<template #button-icon>
+				<Cog :size="20" />
+			</template>
+		</AppNavigationSelect>
+		<NcAppNavigationItem :title="t('biblio', 'Lend/Return')" to="/lend-return">
+			<template #icon>
+				<SwapVertical :size="20" />
+			</template>
+		</NcAppNavigationItem>
+		<NcAppNavigationItem :title="t('biblio', 'Mediums')" to="/mediums">
+			<template #icon>
+				<Bookshelf :size="20" />
+			</template>
+		</NcAppNavigationItem>
+		<NcAppNavigationItem :title="t('biblio', 'Customers')" to="/customers">
+			<template #icon>
+				<AccountMultiple :size="20" />
+			</template>
+		</NcAppNavigationItem>
 	</NcAppNavigation>
 </template>
 <script>
 import AppNavigationSelect from "./AppNavigationSelect.vue";
 import NcAppNavigation from '@nextcloud/vue/dist/Components/NcAppNavigation'
 import NcAppNavigationItem from '@nextcloud/vue/dist/Components/NcAppNavigationItem'
+import NcAppNavigationSpacer from '@nextcloud/vue/dist/Components/NcAppNavigationSpacer.js'
+
+import SwapVertical from 'vue-material-design-icons/SwapVertical';
+import AccountMultiple from 'vue-material-design-icons/AccountMultiple';
+import Bookshelf from 'vue-material-design-icons/Bookshelf';
+import LibraryShelves from 'vue-material-design-icons/LibraryShelves';
+import Cog from 'vue-material-design-icons/Cog';
 
 export default {
 	components: {
 		AppNavigationSelect,
 		NcAppNavigation,
 		NcAppNavigationItem,
+		NcAppNavigationSpacer,
+		SwapVertical,
+		AccountMultiple,
+		Bookshelf,
+		LibraryShelves,
+		Cog,
 	},
 };
 </script>
