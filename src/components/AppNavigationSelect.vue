167dd98bcd7bd6b36f45950f66630463167a82d7
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-09-22 00:16:32 +0000

    started implementing library selector

---
+++ src/components/AppNavigationSelect.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="navigationSelectContainer">
+        <vueSelect class="navigationSelectSelect" :options="['test', 'test 2']" />
+    </div>
+</template>
+
+<script>
+import vueSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+//import NcSelect from '@nextcloud/vue/dist/Components/NcSelect.js';
+
+export default {
+	components: {
+		vueSelect,
+	},
+};
+</script>
+
+<style scoped>
+.navigationSelectContainer {
+    margin-left: 22px;
+    width: calc(100% - 44px);
+    margin-right: 22px;
+}
+
+.navigationSelectSelect {
+    min-width: 100%;
+    width: 100%;
+}
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,17 +1,19 @@
 <template>
 	<NcAppNavigation>
+		<AppNavigationSelect />
 		<NcAppNavigationItem :title="t('biblio', 'Lend/Return')" to="/lend-return" icon="icon-medium" />
 		<NcAppNavigationItem :title="t('biblio', 'Mediums')" to="/mediums" icon="icon-category-enabled" />
 		<NcAppNavigationItem :title="t('biblio', 'Customers')" to="/customers" icon="icon-category-enabled" />
 	</NcAppNavigation>
 </template>
 <script>
-
+import AppNavigationSelect from "./AppNavigationSelect.vue";
 import NcAppNavigation from '@nextcloud/vue/dist/Components/NcAppNavigation'
 import NcAppNavigationItem from '@nextcloud/vue/dist/Components/NcAppNavigationItem'
 
 export default {
 	components: {
+		AppNavigationSelect,
 		NcAppNavigation,
 		NcAppNavigationItem,
 	},
