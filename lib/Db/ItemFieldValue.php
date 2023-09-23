f080601172d253a3d35cd779051d8156799bde2e
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2023-09-23 21:22:33 +0000

    adapted DB Entities

--- lib/Db/ItemFieldValue.php
+++ lib/Db/ItemFieldValue.php
@@ -7,21 +7,21 @@ use JsonSerializable;
 use OCP\AppFramework\Db\Entity;
 
 class ItemField extends Entity implements JsonSerializable {
-	protected $collectionId;
-	protected $name;
-	protected $type;
+	protected $itemId;
+	protected $fieldId;
 	protected $value;
 
 	public function __construct() {
-        $this->addType('collectionId','integer');
+        $this->addType('itemId','integer');
+        $this->addType('fieldId','integer');
     }
 
 	public function jsonSerialize(): array {
 		return [
 			'id' => $this->id,
-            'collectionId' => $this->collectionId,
-			'name' => $this->name,
-			'type' => $this->type,
+            'itemId' => $this->itemId,
+			'fieldId' => $this->fieldId,
+			'value' => $this->value
 		];
 	}
 }
