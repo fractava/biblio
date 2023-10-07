14f9186b8034af94a8a543677de54c7acd1c80b3
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-07 04:14:22 +0200

    improved ApiObjectMapper

--- lib/Traits/ApiObjectMapper.php
+++ lib/Traits/ApiObjectMapper.php
@@ -27,17 +27,27 @@ trait ApiObjectMapper {
 		}
     }
 
-    public function handleStringFilter(IDBConnection $db, IQueryBuilder $qb, ?array $filter, string $column, bool $and = true) {
+    public function handleStringFilterExpr(IDBConnection $db, IQueryBuilder $qb, ?array $filter, string $column) {
         if(isset($filter) && is_string($filter["operand"]) && $filter["operand"] !== '') {
             if($filter["operator"] === "=") {
-				$expression = $qb->expr()->eq($column, $qb->createNamedParameter($filter["operand"], IQueryBuilder::PARAM_STR));
-			} else if($filter["operator"] === "includes") {
-                $expression = $qb->expr()->iLike($column, $qb->createNamedParameter('%' . $db->escapeLikeParameter($filter["operand"]) . '%'));
+				return $qb->expr()->eq($column, $qb->createNamedParameter("\"" . $filter["operand"] . "\"", IQueryBuilder::PARAM_STR));
+			} else if($filter["operator"] === "contains") {
+                return $qb->expr()->iLike($column, $qb->createNamedParameter('%' . $db->escapeLikeParameter($filter["operand"]) . '%'));
             } else if($filter["operator"] === "startsWith") {
-                $expression = $qb->expr()->iLike($column, $qb->createNamedParameter($db->escapeLikeParameter($filter["operand"]) . '%'));
+                return $qb->expr()->iLike($column, $qb->createNamedParameter($db->escapeLikeParameter("\"" . $filter["operand"]) . '%'));
             } else if($filter["operator"] === "endsWith") {
-                $expression = $qb->expr()->iLike($column, $qb->createNamedParameter('%' . $db->escapeLikeParameter($filter["operand"])));
+                return $qb->expr()->iLike($column, $qb->createNamedParameter('%' . $db->escapeLikeParameter($filter["operand"] . "\"")));
+            } else {
+                throw new \Error("unknown operator");
             }
+        } else {
+            throw new \Error("wrong handleStringFilterExpr parameters");
+        }
+    }
+
+    public function handleStringFilter(IDBConnection $db, IQueryBuilder $qb, ?array $filter, string $column, bool $and = true) {
+        if(isset($filter) && is_string($filter["operand"]) && $filter["operand"] !== '') {
+			$expression = $this->handleStringFilterExpr($db, $qb, $filter, $column);
 
             if($and) {
                 $qb->andWhere($expression);
