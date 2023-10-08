7f230435dda4ffbdd941e935a8c714f4a9d97106
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-09 01:22:17 +0200

    migrate long text value to NcRichContenteditable and NcRichText

--- src/components/Fields/LongTextFieldValue.vue
+++ src/components/Fields/LongTextFieldValue.vue
@@ -1,25 +1,32 @@
 <template>
 	<div class="field__content">
 		<div v-if="allowValueEdit">
-			<textarea ref="textarea"
-				:placeholder="placeholder"
-				:disabled="!allowValueEdit"
+			<NcRichContenteditable :auto-complete="() => {}"
 				:value="value"
-				class="field__text"
-				minlength="1"
-				@input="onInput"
-				@keypress="autoSizeText" />
+				:multiline="true"
+				@update:value="onInput" />
 		</div>
-		<span v-else style="white-space: pre;">
-			{{ value }}
-		</span>
+		<div v-else>
+			<NcRichText :text="value"
+				:autolink="true"
+				:use-markdown="true" />
+		</div>
 	</div>
 </template>
 
 <script>
+import debounceFn from "debounce-fn";
+
+import NcRichContenteditable from "@nextcloud/vue/dist/Components/NcRichContenteditable.js";
+import NcRichText from "@nextcloud/vue/dist/Components/NcRichText.js";
+
 import FieldValue from "../mixins/FieldValue.js";
 
 export default {
+	components: {
+		NcRichContenteditable,
+		NcRichText,
+	},
 	mixins: [FieldValue],
 	props: {
 		value: {
@@ -27,68 +34,16 @@ export default {
 			default: "",
 		},
 	},
-	data() {
-		return {
-			height: 1,
-		};
-	},
-	watch: {
-		allowValueEdit() {
-			this.autoSizeText();
-		},
-	},
-	mounted() {
-		this.autoSizeText();
-	},
 	methods: {
-		onInput() {
-			const textarea = this.$refs.textarea;
-			this.$emit("update:value", textarea.value);
-			this.autoSizeText();
-		},
-		autoSizeText() {
-			if (this.allowValueEdit) {
-				const refresh = () => {
-					const textarea = this.$refs.textarea;
-					console.log(textarea);
-					textarea.style.cssText = "height:auto; padding:0";
-					textarea.style.cssText = `height: ${textarea.scrollHeight + 20}px`;
-				};
-				if (this.$refs.textarea) {
-					refresh();
-				} else {
-					this.$nextTick(refresh);
-				}
-			}
-		},
-		onKeydownCtrlEnter(event) {
-			this.$emit("keydown", event);
-		},
+		onInput: debounceFn(function(newValue) {
+			this.$emit("update:value", newValue);
+		}, { wait: 500 }),
 	},
 };
 </script>
-
-<style lang="scss" scoped>
-.field__text {
-	// make sure height calculations are correct
-	box-sizing: content-box !important;
+<style scoped>
+.field__content {
 	width: 100%;
-	min-width: 100%;
-	max-width: 100%;
-	min-height: 44px;
-	margin: 0;
-	padding: 6px 0;
-	border: 0;
-	border-bottom: 1px dotted var(--color-border-dark);
-	border-radius: 0;
-	resize: none;
-	font-size: 14px;
-
-	&:disabled {
-		// Just overrides Server CSS-Styling for disabled inputs. -> Not Good??
-		background-color: var(--color-main-background);
-		color: var(--color-main-text);
-	}
+    white-space: initial;
 }
-
 </style>
