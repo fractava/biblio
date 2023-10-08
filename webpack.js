b1a696c77dca44293ec5a2c8dfc5bf9e84517d97
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-08 04:47:00 +0200

    added bundle analyzer to webpack config

--- webpack.js
+++ webpack.js
@@ -1,3 +1,6 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 
 module.exports = webpackConfig
