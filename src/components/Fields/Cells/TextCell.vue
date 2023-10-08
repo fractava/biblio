34b9ce102ece8d85f8a9e851c5b4ec4dbf0174db
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-09 01:35:23 +0200

    limited text length in text cell

--- src/components/Fields/Cells/TextCell.vue
+++ src/components/Fields/Cells/TextCell.vue
@@ -1,5 +1,7 @@
 <template>
-	<span>{{ value }}</span>
+	<div>
+		<span>{{ limitedValue }}</span>
+	</div>
 </template>
 
 <script>
@@ -7,5 +9,14 @@ import cellMixin from "./cellMixin";
 
 export default {
 	mixins: [cellMixin],
+	computed: {
+		limitedValue() {
+			if (this.value.length > 30) {
+				return this.value.slice(0, 27) + "...";
+			} else {
+				return this.value;
+			}
+		},
+	},
 };
 </script>
