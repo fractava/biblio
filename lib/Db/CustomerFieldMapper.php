e52918e20b447d37040701b2b67545139959623c
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-10 23:57:20 +0200

    added customer field mapper

---
+++ lib/Db/CustomerFieldMapper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OCA\Biblio\Db;
+
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\Entity;
+use OCP\AppFramework\Db\QBMapper;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+
+class CustomerFieldMapper extends QBMapper {
+	const TABLENAME = 'biblio_customer_fields';
+
+	public function __construct(IDBConnection $db) {
+		parent::__construct($db, self::TABLENAME, CustomerField::class);
+	}
+
+	/**
+	 * @param int $id
+	 * @param int $collectionId
+	 * @return Entity|CustomerField
+	 * @throws \OCP\AppFramework\Db\MultipleObjectsReturnedException
+	 * @throws DoesNotExistException
+	 */
+	public function find(int $id, int $collectionId): CustomerField {
+		/* @var $qb IQueryBuilder */
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('*')
+			->from(self::TABLENAME)
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($id, IQueryBuilder::PARAM_INT)))
+			->andWhere($qb->expr()->eq('collection_id', $qb->createNamedParameter($collectionId, IQueryBuilder::PARAM_INT)));
+		
+		return $this->findEntity($qb);
+	}
+
+	/**
+	 * @param int $collectionId
+	 * @return array
+	 */
+	public function findAll(int $collectionId): array {
+		/* @var $qb IQueryBuilder */
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('*')
+			->from(self::TABLENAME)
+			->where($qb->expr()->eq('collection_id', $qb->createNamedParameter($collectionId, IQueryBuilder::PARAM_INT)));
+		
+		return $this->findEntities($qb);
+	}
+}
