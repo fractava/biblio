4cdc7d39b883d5183dfbb66b5189540f24db6efa
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-10 23:55:33 +0200

    added customer field Entity

---
+++ lib/Db/CustomerField.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\Biblio\Db;
+
+use JsonSerializable;
+
+use OCP\AppFramework\Db\Entity;
+
+class CustomerField extends Entity implements JsonSerializable {
+	protected $collectionId;
+	protected $name;
+	protected $type;
+	protected $settings;
+	protected $includeInList;
+
+	public function __construct() {
+        $this->addType('collectionId','integer');
+		$this->addType('includeInList','bool');
+    }
+
+	public function jsonSerialize(): array {
+		return [
+			'id' => $this->id,
+            'collectionId' => $this->collectionId,
+			'name' => $this->name,
+			'type' => $this->type,
+			'settings' => $this->settings,
+			'includeInList' => $this->includeInList,
+		];
+	}
+}
