431c6c4eb7cffd9d078c315671e764cf7c167cb6
Jonathan Treffler <mail@jonathan-treffler.de>
2023-10-11 01:31:18 +0200

    added CustomerFieldValueMapper

--- lib/Db/CustomerFieldValueMapper.php
+++ lib/Db/CustomerFieldValueMapper.php
@@ -10,40 +10,40 @@ use OCP\IDBConnection;
 
 use OCA\Biblio\Traits\ApiObjectMapper;
 
-class ItemFieldValueMapper extends QBMapper {
+class CustomerFieldValueMapper extends QBMapper {
 	use ApiObjectMapper;
 
-	const TABLENAME = 'biblio_item_fields_values';
+	const TABLENAME = 'biblio_customer_fields_values';
 
-	const FIELDS_TABLENAME = 'biblio_item_fields';
+	const FIELDS_TABLENAME = 'biblio_customer_fields';
 
 	public function __construct(IDBConnection $db) {
-		parent::__construct($db, self::TABLENAME, ItemFieldValue::class);
+		parent::__construct($db, self::TABLENAME, CustomerFieldValue::class);
 	}
 
 	/**
 	 * @param array $parameters
-	 * @return Entity|ItemFieldValue
+	 * @return Entity|CustomerFieldValue
 	 * @throws \OCP\AppFramework\Db\MultipleObjectsReturnedException
 	 * @throws DoesNotExistException
 	 */
-	public function find(array $parameters): ItemFieldValue {
+	public function find(array $parameters): CustomerFieldValue {
 		/* @var $qb IQueryBuilder */
 		$qb = $this->db->getQueryBuilder();
 
 		$findById = array_key_exists("id", $parameters);
-		$findByItemIdFieldId = array_key_exists("itemId", $parameters) && array_key_exists("fieldId", $parameters);
+		$findByCustomerIdFieldId = array_key_exists("customerId", $parameters) && array_key_exists("fieldId", $parameters);
 
 		$qb->select('*')
 			->from(self::TABLENAME);
 
 		if ($findById) {
 			$qb->where($qb->expr()->eq('id', $qb->createNamedParameter($parameters["id"], IQueryBuilder::PARAM_INT)));
-		} else if ($findByItemIdFieldId) {
-			$qb->where($qb->expr()->eq('item_id', $qb->createNamedParameter($parameters["itemId"], IQueryBuilder::PARAM_INT)))
+		} else if ($findByCustomerIdFieldId) {
+			$qb->where($qb->expr()->eq('customer_id', $qb->createNamedParameter($parameters["customerId"], IQueryBuilder::PARAM_INT)))
             	->andWhere($qb->expr()->eq('field_id', $qb->createNamedParameter($parameters["fieldId"], IQueryBuilder::PARAM_INT)));
 		} else {
-			throw new Exception("Invalid parameters supplied to ItemFieldValueMapper->find");
+			throw new Exception("Invalid parameters supplied to CustomerFieldValueMapper->find");
 		}
 
 		return $this->findEntity($qb);
@@ -51,9 +51,9 @@ class ItemFieldValueMapper extends QBMapper {
 
 	/**
 	 * @param array $parameters
-	 * @return ItemFieldValueFieldCombination
+	 * @return CustomerFieldValueFieldCombination
 	 */
-	public function findIncludingField(array $parameters): ItemFieldValueFieldCombination {
+	public function findIncludingField(array $parameters): CustomerFieldValueFieldCombination {
 		if (array_key_exists("collectionId", $parameters)) {
 			/* @var $qb IQueryBuilder */
 			$qb = $this->db->getQueryBuilder();
@@ -63,20 +63,20 @@ class ItemFieldValueMapper extends QBMapper {
 				->from(self::TABLENAME, 'v');
 
 			$findById = array_key_exists("id", $parameters);
-			$findByItemIdFieldId = array_key_exists("itemId", $parameters) && array_key_exists("fieldId", $parameters);
+			$findByCustomerIdFieldId = array_key_exists("customerId", $parameters) && array_key_exists("fieldId", $parameters);
 
 			if ($findById) {
 				$joinON = [
 					$qb->expr()->eq('v.id', $qb->createNamedParameter($parameters["id"], IQueryBuilder::PARAM_INT)),
 					$qb->expr()->eq('v.field_id', 'f.id'),
 				];
-			} else if ($findByItemIdFieldId) {
+			} else if ($findByCustomerIdFieldId) {
 				$joinON = [
-					$qb->expr()->eq('v.item_id', $qb->createNamedParameter($parameters["itemId"], IQueryBuilder::PARAM_INT)),
+					$qb->expr()->eq('v.customer_id', $qb->createNamedParameter($parameters["customerId"], IQueryBuilder::PARAM_INT)),
 					$qb->expr()->eq('v.field_id', $qb->createNamedParameter($parameters["fieldId"], IQueryBuilder::PARAM_INT)),
 				];
 			} else {
-				throw new Exception("Invalid parameters supplied to ItemFieldValueMapper->findIncludingField");
+				throw new Exception("Invalid parameters supplied to CustomerFieldValueMapper->findIncludingField");
 			}
 			
 			$qb->rightJoin('v', self::FIELDS_TABLENAME, 'f', $qb->expr()->andX(...$joinON));
@@ -85,38 +85,38 @@ class ItemFieldValueMapper extends QBMapper {
 
 			if ($findById) {
 				$qb->where($collectionExpr);
-			} else if ($findByItemIdFieldId) {
+			} else if ($findByCustomerIdFieldId) {
 				$qb->where($qb->expr()->eq('f.id', $qb->createNamedParameter($parameters["fieldId"], IQueryBuilder::PARAM_INT)));
 				$qb->andWhere($collectionExpr);
 			}
 
-			$result = ItemFieldValueFieldCombination::fromRow($this->findOneQuery($qb));
+			$result = CustomerFieldValueFieldCombination::fromRow($this->findOneQuery($qb));
 			
 			return $result;
 		} else {
-			throw new Exception("Invalid parameters supplied to ItemFieldValueMapper->findIncludingField");
+			throw new Exception("Invalid parameters supplied to CustomerFieldValueMapper->findIncludingField");
 		}
 	}
 
 	/**
-	 * @param int $itemId
+	 * @param int $customerId
 	 * @return array
 	 */
-	public function findAll(int $itemId, ?array $filters): array {
+	public function findAll(int $customerId, ?array $filters): array {
 		/* @var $qb IQueryBuilder */
 		$qb = $this->db->getQueryBuilder();
 		$qb->select('*')
 			->from(self::TABLENAME)
-			->where($qb->expr()->eq('item_id', $qb->createNamedParameter($itemId, IQueryBuilder::PARAM_INT)));
+			->where($qb->expr()->eq('customer_id', $qb->createNamedParameter($customerId, IQueryBuilder::PARAM_INT)));
 		
 		return $this->findEntities($qb);
 	}
 
     /**
-	 * @param int $itemId
+	 * @param int $customerId
 	 * @return array
 	 */
-	public function findAllIncludingFields(int $collectionId, int $itemId, ?array $filters, ?int $limit = null, ?int $offset = null): array {
+	public function findAllIncludingFields(int $collectionId, int $customerId, ?array $filters, ?int $limit = null, ?int $offset = null): array {
 		/* @var $qb IQueryBuilder */
 		$qb = $this->db->getQueryBuilder();
         $qb->select('*')
@@ -124,7 +124,7 @@ class ItemFieldValueMapper extends QBMapper {
             ->from(self::TABLENAME, 'v')
             ->rightJoin('v', self::FIELDS_TABLENAME, 'f', $qb->expr()->andX(
                 $qb->expr()->eq('v.field_id', 'f.id'),
-                $qb->expr()->eq('v.item_id', $qb->createNamedParameter($itemId, IQueryBuilder::PARAM_INT))
+                $qb->expr()->eq('v.customer_id', $qb->createNamedParameter($customerId, IQueryBuilder::PARAM_INT))
             ))
 			->where($qb->expr()->eq('f.collection_id', $qb->createNamedParameter($collectionId, IQueryBuilder::PARAM_INT)));
 
@@ -143,7 +143,7 @@ class ItemFieldValueMapper extends QBMapper {
 		try {
 			$entities = [];
 			while ($row = $result->fetch()) {
-				$entities[] = ItemFieldValueFieldCombination::fromRow($row);
+				$entities[] = CustomerFieldValueFieldCombination::fromRow($row);
 			}
 			return $entities;
 		} finally {
