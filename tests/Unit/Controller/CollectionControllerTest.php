89298579f557f6b3e229edb6079670926f9b1e01
Jonathan Treffler <mail@jonathan-treffler.de>
2023-11-06 13:51:08 +0000

    improved CollectionControllerTest

--- tests/Unit/Controller/CollectionControllerTest.php
+++ tests/Unit/Controller/CollectionControllerTest.php
@@ -4,8 +4,11 @@ namespace OCA\Biblio\Tests\Unit\Controller;
 
 use PHPUnit\Framework\TestCase;
 use OCP\AppFramework\Http;
+use OCP\IRequest;
 
+use OCA\Biblio\Service\CollectionService;
 use OCA\Biblio\Errors\CollectionNotFound;
+use OCA\Biblio\Controller\CollectionController;
 
 class CollectionControllerTest extends TestCase {
 	protected $controller;
@@ -13,12 +16,12 @@ class CollectionControllerTest extends TestCase {
 	protected $userId = 'tomtester';
 	protected $request;
 
-	protected function setUp() {
-		$this->request = $this->getMockBuilder(\OCP\IRequest::class)->getMock();
-		$this->service = $this->getMockBuilder(\OCA\Biblio\Service\CollectionService::class)
+	protected function setUp(): void {
+		$this->request = $this->getMockBuilder(IRequest::class)->getMock();
+		$this->service = $this->getMockBuilder(CollectionService::class)
 			->disableOriginalConstructor()
 			->getMock();
-		$this->controller = new \OCA\Biblio\Controller\CollectionController(
+		$this->controller = new CollectionController(
 			$this->request, $this->service, $this->userId
 		);
 	}
