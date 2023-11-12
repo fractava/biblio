4a9637e119b1647e04c46f21ab2a4bf141cf43ce
Jonathan Treffler <mail@jonathan-treffler.de>
2023-11-12 23:01:33 +0000

    started implementing timestamp cell (will only work after nextcloud vue update)

---
+++ src/components/TimestampCell.vue
@@ -0,0 +1,21 @@
+<template>
+	<NcDateTime :timestamp="value" :format="timeFormat" :relative-time="true" />
+</template>
+<script>
+import NcDateTime from "@nextcloud/vue/dist/Components/NcDateTime.js";
+
+export default {
+	components: {
+		NcDateTime,
+	},
+	props: ["value"],
+	data() {
+		return {
+			timeFormat: {
+				dateStyle: "short",
+				timeStyle: "full",
+			},
+		};
+	},
+};
+</script>
