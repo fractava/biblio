67ffa24ca6fb2813a2aedb9611737182ea1b90d3
Jonathan Treffler <mail@jonathan-treffler.de>
2023-11-12 23:01:04 +0000

    started implementing history entry description cell

--- src/components/HistoryEntryDescriptionCell.vue
+++ src/components/HistoryEntryDescriptionCell.vue
@@ -1,1 +1,27 @@
-<template></template>
+<template>
+	<div>
+		{{ message }}
+	</div>
+</template>
+<script>
+import { mapStores } from "pinia";
+
+import { useNomenclatureStore } from "../store/nomenclature.js";
+
+export default {
+	props: ["value"],
+	computed: {
+		...mapStores(useNomenclatureStore),
+		message() {
+			switch (this.value?.type) {
+			case "customer.create":
+				return this.nomenclatureStore.customerCreated(this.value?.properties?.after?.name);
+			case "item.create":
+				return this.nomenclatureStore.itemCreated(this.value?.properties?.after?.title);
+			default:
+				return this.value?.type;
+			}
+		},
+	},
+};
+</script>
