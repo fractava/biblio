a3508cadd4299c2139b40f565e25aa9b3f65b785
Jonathan Treffler <mail@jonathan-treffler.de>
2024-03-24 13:45:08 +0000

    added button group component

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+		<slot />
+	</div>
+</template>
+<style>
+.container {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+
+	.button-vue:not(:first-child) {
+		border-top-left-radius: 0px;
+		border-bottom-left-radius: 0px;
+	}
+
+	.button-vue:not(:last-child) {
+		border-top-right-radius: 0px;
+		border-bottom-right-radius: 0px;
+	}
+}
+</style>
